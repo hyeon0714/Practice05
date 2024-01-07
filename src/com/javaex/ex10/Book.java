bd9d82d2723bf0aaa16d2c9926272026fafb3a4a
user <user@DESKTOP-KVO6FOC>
2024-01-07 22:39:39 +0900

    .

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -6,6 +6,7 @@ public class Book {
     private String title;
     private String author;
     private int stateCode;
+    private String state;
     
     
     public Book(int bookNo, String title, String author) {
@@ -13,56 +14,50 @@ public class Book {
     	this.title=title;
     	this.author=author;
     	this.stateCode=1;
+    	this.state="재고있음";
     }
-    
-    
-    
-    
-    
-    
-    public int getBookNo() {
+
+
+	public int getBookNo() {
 		return bookNo;
 	}
+
 
 	public void setBookNo(int bookNo) {
 		this.bookNo = bookNo;
 	}
 
+
 	public String getTitle() {
 		return title;
 	}
+
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+
 	public String getAuthor() {
 		return author;
 	}
 
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-
-	public int getStateCode() {
-		return stateCode;
-	}
-
-	public void setStateCode(int stateCode) {
-		this.stateCode = stateCode;
-	}
-
-
-
-	public void rent() {
-		this.stateCode=0;
-		System.out.println(title+"이(가) 대여되었습니다");
-	}
-	
-
-	
-	
-	public void displayBookInfo() {
-    	System.out.println(bookNo+" 책 제목:"+title+", 작가:"+author+", 대여유무:"+stateCode);
+    
+    public void rent(int stateCode) {
+    	if(stateCode==0) {
+    		state="대여중";
+    	}
+    	else {
+    		state="재고있음";
+    	}
     }
+    
+    public void print() {
+    	System.out.println(bookNo+"책제목:"+title+", 작가:"+author+", 대여유무:"+state);
+    }
+  
 }
--- src/com/javaex/ex10/BookShop.java
+++ src/com/javaex/ex10/BookShop.java
@@ -8,7 +8,7 @@ public class BookShop {
         Book[] books = new Book[10];
 
         books[0] = new Book(1, "트와일라잇", "스테파니메이어");
-        books[1] = new Book(2, "뉴문", "스테파니메이어");
+        books[1] = new Book(2, "뉴문", "스테파니메이어");  
         books[2] = new Book(3, "이클립스", "스테파니메이어");
         books[3] = new Book(4, "브레이킹던", "스테파니메이어");
         books[4] = new Book(5, "아리랑", "조정래");
@@ -24,12 +24,13 @@ public class BookShop {
         Scanner scanner = new Scanner(System.in);
         System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
         int num = scanner.nextInt();
-       
+        num = num-1;//배열이 0부터 시작이고 순번이 1부터 시작이므로 순서를 맞춰줘야한다
+        
 
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
         // 코드작성
-        
-        
+        System.out.println(books[num].getTitle()+"이(가) 대여되었습니다");
+        books[num].rent(0);//books[]별 rent값을 바꾸어 state 상태가 바뀐다
 
         System.out.println("*****도서 정보 출력하기******");
         displayBookInfo(books);
@@ -37,9 +38,14 @@ public class BookShop {
         scanner.close();
     }
 
-    //(2)전달받은 배열을 모두 출력하는 메소드
+    //(2)전달받은 배열을 모두 출력하는 메소드// 디스플레이 출려 메소드가 Book에 없고 여기 있음
     private static void displayBookInfo(Book[] books) {
         
     	//코드작성
+    	for(int i=0; i<books.length; i++) {
+    		books[i].print();
+    	}
+    	
     }
+    
 }
